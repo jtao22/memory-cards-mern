7985eb95259d3a1cd1641a3be94da4d0a2336494
Jonathan Tao <68477691+jtao22@users.noreply.github.com>
2020-11-24 00:06:32 -0800

    Create index.js

---
+++ client/src/api/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const URL = 'https://memory-cards-mern.herokuapp.com/posts';
+export const fetchPosts = () => axios.get(URL);
+export const createPost = (newPost) => axios.post(URL, newPost);
+export const changePost = (id, updatedPost) => axios.patch(`${URL}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${URL}/${id}`);
+export const likePost = (id) => axios.patch(`${URL}/${id}/likePost`);
